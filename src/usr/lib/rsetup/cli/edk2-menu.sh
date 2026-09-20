669b9acf530adb1fbd0e2b9922faffcfd98edfd6
Jiali Chen <chenjiali@radxa.com>
2026-09-20 17:56:44 +0800

    refactor: remove redundant overlay disable wrappers

--- src/usr/lib/rsetup/cli/edk2-menu.sh
+++ src/usr/lib/rsetup/cli/edk2-menu.sh
@@ -135,12 +135,3 @@ enable_edk2_overlays() {
     enable_overlay_general "$@" || return $?
     update_entry_overlays
 }
-
-disable_edk2_overlay() {
-    __parameter_count_at_least_check 1 "$@"
-    if ! load_edk2_setting; then
-        return 1
-    fi
-    disable_overlay_general "$@" || return $?
-    update_entry_overlays
-}
--- src/usr/lib/rsetup/cli/u-boot-menu.sh
+++ src/usr/lib/rsetup/cli/u-boot-menu.sh
@@ -59,12 +59,3 @@ enable_u-boot_overlays() {
     enable_overlay_general "$@" || return $?
     u-boot-update
 }
-
-disable_u-boot_overlay() {
-    __parameter_count_at_least_check 1 "$@"
-    if ! load_u-boot_setting; then
-        return 1
-    fi
-    disable_overlay_general "$@" || return $?
-    u-boot-update
-}
